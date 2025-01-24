3e37a8fa0118f14478bbc6b20640ffab1c1bd861
mouadHILLALI <mauadtoto001@gmail.com>
2025-01-24 12:16:53 +0100

    commiting the backup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthService } from './auth/auth.service';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,25 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './register/register.component';
 import { AuthComponent } from './auth.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     RegisterComponent,
     AuthComponent,
-    LoginComponent,
-  
+    LoginComponent
   ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    ReactiveFormsModule,
-    LogoutComponent
-  ]
+  imports:[AuthRoutingModule,ReactiveFormsModule],
 })
 export class AuthModule { }
